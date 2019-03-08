fee9621c656960ddd3cbd00b0f327188bc7fa352
Eric Storey <estorey@gmail.com>
2019-03-08 20:37:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,12 +18,8 @@ def using_shift(a)
   a.shift
 end
 
-def shift_with_args(a, num)
-  until num==0 
-    a.shift 
-    num-=1
-  end
-  a
+def shift_with_args(a)
+  a.shift(2)
 end
 
 def using_concat(a1, a2)
