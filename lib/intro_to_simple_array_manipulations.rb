02008b53f356fdc682967073bf8bf46f770e0851
Eric Storey <estorey@gmail.com>
2019-03-08 16:41:21 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,55 @@
+def using_push(a, str)
+  a.push(str)
+end
+
+def using_unshift(a, str)
+  a.unshift(str)
+end
+
+def using_pop(a)
+  a.pop
+end
+
+def pop_with_args(a, num)
+  until num==0 
+    a.pop 
+    num-=1
+  end
+  a
+end
+
+def using_shift(a)
+  shift.a
+end
+
+def shift_with_args(a, num)
+  until num==0 
+    a.shift 
+    num-=1
+  end
+  a
+end
+
+def using_concat(a1, a2)
+  a1.concat(a2)
+end
+
+def using_insert(a, ins)
+  a.insert(ins, 4)
+end
+
+def using_uniq(a)
+  a.uniq
+end
+
+def using_flatten
+
+end
+
+def
+
+end
+
+def
+
+end
